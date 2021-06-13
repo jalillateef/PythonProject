2634f35cfa1ea0ff1d8b312c42c0e34c014d9aa9
Jalil Lateef <jteef@Jalils-MacBook-Pro.local>
2021-06-13 18:49:33 -0400

    Initial commit python project

---
+++ guessthenumber.py
@@ -0,0 +1,7 @@
+from random import randint
+#Ask the user for their name
+name = input("Enter name: ")
+#Welcome the user
+print(name + ", welcome to Guess The Number!")
+#Give the user instructions for the game
+print("Instructions: ")
