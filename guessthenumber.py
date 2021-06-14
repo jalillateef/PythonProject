378b9bf21a385213503818279f2b7185c7bd68d7
Jalil Lateef <jteef@Jalils-MacBook-Pro.local>
2021-06-14 04:16:02 -0400

    third commit

--- guessthenumber.py
+++ guessthenumber.py
@@ -6,10 +6,11 @@ print(name + ", welcome to Guess The Number!")
 
 #Give the user instructions for the game
 print("""Instructions: The object of the game is to guess the secret number from 1-100. 
-You have a total of 7 guesses. You will get a new hint every time you guess wrong, but
-your number og guesses will be reduced by 1.""")
+    You have a total of 7 guesses. You will get a new hint every time you guess wrong, but
+    your number of guesses will be reduced by 1.""")
 
-#Ask the user if they are ready
+    #Ask the user if they are ready
+
 while True:
     answer = input("Are you ready? Reply Yes or No: ").lower()
     if answer.startswith('y'):
@@ -21,17 +22,43 @@ while True:
     else:
         print("You have to choose Yes or No.")
 
+#Import the random module (To generate random numbers between 1 - 100)
 import random
-n = random.randint(1, 99)
+
+#Set the random number range
+n = random.randint(1, 10)
+
+#Set a variable for starting number of tries
 tries_left = 6
-guess = int(input("Enter an integer from 1 to 100: "))
-while n != guess:
-    if guess > n:
-        print("Too high, try again.")
-        tries_left -= 1
-        print ("Tries left: " + str(tries_left))
-        guess = int(input("Enter an integer from 1 to 100: "))
-    # elif guess < n:
+while True:
+    integer = ()
+            #Ask the user to pick a number and then control flow.
+    guess = int(input("Enter an integer from 1 to 10: "))
+    while n != guess:
+        if guess > n:
+                print("Too high, try again.")
+                tries_left -= 1
+                print ("Tries left: " + str(tries_left))
+                guess = int(input("Enter an integer from 1 to 10: "))
+        elif guess < n:
+                print("Too low, try again.")
+                tries_left -= 1
+                print ("Tries left: " + str(tries_left))
+                guess = int(input("Enter an integer from 1 to 10: "))
+        if tries_left == 1:
+                print("GAME OVER!!!")
+            
+        if guess == n:
+                winner_play_again = input("WINNER! CONGRATULATIONS! Would you like to play again?")
+                if winner_play_again.startswith('y'):
+                    response = input()
+                elif not winner_play_again.startswith('y'):
+                    print("Goodbye!")
+                    break
+
+
+    
+    
         
 
 
