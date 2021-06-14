c373c1f208b858e55e167c1766870fd9764bee69
Jalil Lateef <jteef@Jalils-MacBook-Pro.local>
2021-06-14 17:48:45 -0400

    commit number 4

--- guessthenumber.py
+++ guessthenumber.py
@@ -6,8 +6,7 @@ print(name + ", welcome to Guess The Number!")
 
 #Give the user instructions for the game
 print("""Instructions: The object of the game is to guess the secret number from 1-100. 
-    You have a total of 7 guesses. You will get a new hint every time you guess wrong, but
-    your number of guesses will be reduced by 1.""")
+    You have a total of 7 guesses. You will get a new hint every time you guess wrong.""")
 
     #Ask the user if they are ready
 
@@ -26,42 +25,65 @@ while True:
 import random
 
 #Set the random number range
-n = random.randint(1, 10)
+number = random.randint(1, 10)
 
-#Set a variable for starting number of tries
-tries_left = 6
-while True:
-    integer = ()
-            #Ask the user to pick a number and then control flow.
-    guess = int(input("Enter an integer from 1 to 10: "))
-    while n != guess:
-        if guess > n:
-                print("Too high, try again.")
-                tries_left -= 1
-                print ("Tries left: " + str(tries_left))
-                guess = int(input("Enter an integer from 1 to 10: "))
-        elif guess < n:
-                print("Too low, try again.")
-                tries_left -= 1
-                print ("Tries left: " + str(tries_left))
-                guess = int(input("Enter an integer from 1 to 10: "))
-        if tries_left == 1:
-                print("GAME OVER!!!")
+#Set a variable for starting number of guesses
+guesses_remaining = 7
+while guesses_remaining < 8 and guesses_remaining != 0:
+    guess = int(input("I'm thinking of a number from 1 to 10, take a guess: "))
+    guesses_remaining -= 1
+
+    if guess < number:
+        print('Your guess is too low.')
+        print ("guesses remaining " + str(guesses_remaining))
+
+    if guess > number:
+        print('Your guess is too high.')
+        print ("guesses left: " + str(guesses_remaining))
+
+    if guess == number:
+        print("YOU WON!")
+        break
+    
+
+if guesses_remaining == 0:
+    print("YOU LOSE!")
+    number = str(number)
+    print("The number i had in mind was " + number + ".")
+    
+
+
+
+    
+    #         #Ask the user to pick a number and then control flow.
+    # guess = int(input("I'm thinking of a number from 1 to 10: "))
+    # while n != guess:
+    #     if guess > n:
+    #             print("Too high, try again.")
+    #             tries_left -= 1
+    #             print ("Tries left: " + str(tries_left))
+    #             guess = int(input("I'm thinking of a number from 1 to 10: "))
+    #     elif guess < n:
+    #             print("Too low, try again.")
+    #             tries_left -= 1
+    #             print ("Tries left: " + str(tries_left))
+    #             guess = int(input("I'm thinking of a number from 1 to 10: "))
+    #     if tries_left == 1:
+    #             print("GAME OVER!!!")
             
-        if guess == n:
-                winner_play_again = input("WINNER! CONGRATULATIONS! Would you like to play again?")
-                if winner_play_again.startswith('y'):
-                    response = input()
-                elif not winner_play_again.startswith('y'):
-                    print("Goodbye!")
-                    break
-
-
-    
-    
+    #     if guess == n:
+    #             winner_play_again = input("WINNER! CONGRATULATIONS! Would you like to play again?")
+    #             if winner_play_again.startswith('y'):
+    #                 response = input()
+    #             elif not winner_play_again.startswith('y'):
+    #                 print("Goodbye!")
+    #                 break
         
 
 
+            
+            
+                
 
 
 
@@ -72,16 +94,18 @@ while True:
 
 
 
-#Ask user if they are ready
 
-# answer = input('Are you ready? Reply yes or no:').lower()
-# if answer.startswith('y'): 
-#     print("Great!") 
-# elif answer.startswith('n'):
-#     print("Well you better get ready!")
-# else:
-#     print("Sorry I didn't understand that.")
-        
+
+    #     #Ask user if they are ready
+
+    #     # answer = input('Are you ready? Reply yes or no:').lower()
+    #     # if answer.startswith('y'): 
+    #     #     print("Great!") 
+    #     # elif answer.startswith('n'):
+    #     #     print("Well you better get ready!")
+    #     # else:
+    #     #     print("Sorry I didn't understand that.")
+                
 
 
 
